292abb639d8efb760825594fea1f301dcd468c5c
gneris177 <gneris177@gmail.com>
2022-12-16 12:53:07 -0300

    Refactor - Refatorando serviço

--- src/app/users/services/users.service.ts
+++ src/app/users/services/users.service.ts
@@ -3,51 +3,35 @@ import { HttpClient, HttpHeaders } from '@angular/common/http'
 import { Observable } from 'rxjs'
 import { User } from '../users'
 
-const httpOptions = {
-  headers: new HttpHeaders({
-    'Content-Type': 'application/json',
-    Authorization: 'my-auth-token',
-  }),
-}
-
 @Injectable({
   providedIn: 'root',
 })
 export class UsersService {
+  baseUrl: string = 'https://d104-45-167-217-37.sa.ngrok.io'
+
   constructor(private http: HttpClient) {}
 
   getUsers(): Observable<any> {
-    return this.http.get<any>('https://d104-45-167-217-37.sa.ngrok.io/users', {
+    return this.http.get<any>(`${this.baseUrl}/users`, {
       headers: { 'ngrok-skip-browser-warning': 'ok' },
     })
   }
 
   getUser(id: number): Observable<User> {
-    return this.http.get<User>(
-      'https://d104-45-167-217-37.sa.ngrok.io/users/' + id,
-      {
-        headers: { 'ngrok-skip-browser-warning': 'ok' },
-      },
-    )
+    return this.http.get<User>(`${this.baseUrl}/users/${id}`, {
+      headers: { 'ngrok-skip-browser-warning': 'ok' },
+    })
   }
 
   createUser(user: User): Observable<User> {
-    return this.http.post<User>(
-      'https://d104-45-167-217-37.sa.ngrok.io/users',
-      user,
-    )
+    return this.http.post<User>(`${this.baseUrl}/users`, user)
   }
 
   updateUser(id: number, user: User): Observable<User> {
-    return this.http.patch<User>(
-      'https://d104-45-167-217-37.sa.ngrok.io/users/' + id,
-      user,
-    )
+    return this.http.patch<User>(`${this.baseUrl}/users/${id}`, user)
   }
 
   deleteUser(id: number): Observable<any> {
-    return this.http.delete<any>(
-      'https://d104-45-167-217-37.sa.ngrok.io/users/' + id,
-    )
+    return this.http.delete<any>(`${this.baseUrl}/users/${id}`)
   }
 }
